428ec773376362f39acfe42c12d5ae366367b4ed
Rachel Gopman <rachel.gopman@gmail.com>
2025-02-18 20:48:10 -0500

    Add files to GitHub

---
+++ exercise/Menu.js
@@ -0,0 +1,9 @@
+const Menu = () => {
+  return (
+    <div className="menu">
+      <h1>This is the Menu</h1>
+    </div>
+  );
+};
+
+export default Menu;
---
+++ exercise/buttons.js
@@ -0,0 +1,11 @@
+const MyButton = ({ to }) => {
+  return (
+    <a href={`/${to}`}>
+      <button className="my-button">
+        Take me to {to === "" ? "home" : to}
+      </button>
+    </a>
+  );
+};
+
+export default MyButton;
